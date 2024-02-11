06766f8af59a5b528ae03f784235430f17e5e2d0
Ken <ken@kenh.fr>
2024-02-11 18:21:38 +0100

    trans fast

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,10 @@
 # File: whisper.api/app/api/__init__.py
 
 from fastapi import APIRouter
-from .endpoints import users, transcribe
+from .endpoints import users, transcribe, transcribe_fast
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(transcribe.router, prefix="/transcribe", tags=["transcribe"])
+api_router.include_router(transcribe.router, prefix="/transcribe_fast", tags=["transcribe_fast"])
